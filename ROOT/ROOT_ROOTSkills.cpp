03dacc01c285a3265d694e21912bb8353b94ca03
gfh16 <gfh16@mails.tsinghua.edu.cn>
2018-11-08 14:49:38 +0800

    rapidaty and pseudo-rapidity.

---
+++ ROOT/ROOT_ROOTSkills.cpp
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////////////////
+///  This file introducing how to show the percentages of a program
+////////////////////////////////////////////////////////////////////////////////
+======================
+1. 显示程序完成进度
+======================
+if(jentry%100000==0)
+{
+  std::cout << "  Percentage = " << std::fixed << std::setprecision(1) << std::setw(5) << (100*double(jentry)/nentries) << " %";
+  std::cout << "   [";
+  int printindex=0;
+  for(; printindex<int(100*double(jentry)/nentries); printindex+=5) std::cout << "=";
+  for(; printindex<100; printindex+=5) std::cout << " ";
+  std::cout << "]\r"; std::cout.flush();
+}
