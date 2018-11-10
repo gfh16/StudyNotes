bec6d91caf2e147f5bd2eac0428a8a50948fa1b9
Guan, Fenhai <38491382+gfh16@users.noreply.github.com>
2018-11-09 22:57:08 -0500

    Update ROOT_Histograms.cpp

--- ROOT/ROOT_Histograms.cpp
+++ ROOT/ROOT_Histograms.cpp
@@ -4,21 +4,31 @@
 {
 
    ***  //cloning a histogram
-        TH1* hc = (TH1*)h1->Clone();  
+   TH1* hc = (TH1*)h1->Clone();  
    
    ***  // Normalizing histograms
-        Double_t scale = norm/h->Integral();
-        h->Scale(scale);
+   Double_t scale = norm/h->Integral();
+   h->Scale(scale);
    
-        h->Scale(1./h->Integral());
+   h->Scale(1./h->Integral());
    
    ***  // No show statistics box of a histogram
-        h->SetStats(0); 
-        h->SetOptStat(0);
+   h->SetStats(0); 
+   h->SetOptStat(0);
    
    ***  // Muliti Histograms Drawing
-         THStack *hs = new THStack("hs","title");
-         hs->Add(h1);
-         hs->Add(h2);
-         hs->Draw();
+  THStack *hs = new THStack("hs","title");
+  hs->Add(h1);
+  hs->Add(h2);
+  hs->Draw();
+  hs->Draw("pfc nostack");
+  hs->GetXaxis()->SetTitle("E_{Lab}(MeV)");
+  hs->GetXaxis()->SetTitleSize(0.04);
+  hs->GetXaxis()->SetLabelSize(0.03);
+  hs->GetXaxis()->CenterTitle(true);
+  hs->GetXaxis()->SetRangeUser(0,EnergyCut);
+  hs->GetYaxis()->SetTitle("#frac{d^{2}N}{dEd#Omega}(a.u.)");
+  hs->GetYaxis()->SetTitleSize(0.04);
+  hs->GetYaxis()->SetLabelSize(0.03);
+  hs->GetYaxis()->CenterTitle(true);
 }
