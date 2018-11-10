e8ecf27318cbe8f0cdf7055d391be4f51654d3b4
Guan, Fenhai <38491382+gfh16@users.noreply.github.com>
2018-11-09 22:27:20 -0500

    Update ROOT_Draw_Setting.cpp

--- ROOT/ROOT_Draw_Setting.cpp
+++ ROOT/ROOT_Draw_Setting.cpp
@@ -3,18 +3,25 @@
 ///////////////////////////////////////////////////////////////////
 
 {
-
+ ****  //gPAD
   gPad->SetLogx(1);//  same as  gPad->SetLogx(true)
   gPad->SetTicky(1); // set axis grid in four axises
-  
+ 
+
+}
 
 
-
-
-
-
-
-
-
-
+{
+  **** // Set Axis
+  hs->Draw("pfc nostack");
+  hs->GetXaxis()->SetTitle("E_{lab}(MeV)");
+  hs->GetXaxis()->SetTitleSize(0.04);   
+  hs->GetXaxis()->SetLabelSize(0.03);
+  hs->GetXaxis()->CenterTitle(true);
+  hs->GetXaxis()->SetRangeUser(0,EnergyCut);
+  hs->GetYaxis()->SetTitle("#frac{d^{2}N}{dEd#Omega}(a.u.)");
+  hs->GetYaxis()->SetTitleSize(0.04);
+  hs->GetYaxis()->SetLabelSize(0.03);
+  hs->GetYaxis()->CenterTitle(true);
+          
 }
