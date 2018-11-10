f81eec9a94cf9446f7477efc4f0c855b2905ab38
Guan, Fenhai <38491382+gfh16@users.noreply.github.com>
2018-11-09 22:23:43 -0500

    Update ROOT_Histograms.cpp

--- ROOT/ROOT_Histograms.cpp
+++ ROOT/ROOT_Histograms.cpp
@@ -3,7 +3,21 @@
 /////////////////////////////////////////////////////////////
 {
 
-   TH1* hc = (TH1*)h1->Clone();  // cloning a histogram
-
-
+   ***  //cloning a histogram
+        TH1* hc = (TH1*)h1->Clone();  
+   
+   ***  // Normalizing histograms
+        Double_t scale = norm/h->Integral();
+        h->Scale(scale);
+   
+        h->Scale(1./h->Integral());
+   
+   ***  // No show statistics box of a histogram
+        h->SetStats(0); 
+   
+   ***  // Muliti Histograms Drawing
+         THStack *hs = new THStack("hs","title");
+         hs->Add(h1);
+         hs->Add(h2);
+         hs->Draw();
 }
