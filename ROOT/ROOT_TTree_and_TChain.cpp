6d469369b9bb22a521c8e6ed9dbf5fb8c46b3006
Guan, Fenhai <38491382+gfh16@users.noreply.github.com>
2018-11-20 12:11:23 -0500

    Update ROOT_TTree_and_TChain.cpp

--- ROOT/ROOT_TTree_and_TChain.cpp
+++ ROOT/ROOT_TTree_and_TChain.cpp
@@ -2,6 +2,7 @@
 ///// The Usage of TTree
 //////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 **** What is TTree ?  Why we use tree ?
 1. Each object is not written individually, will produce a much smaller file than if the objects were written individually.
 2. Reduces the header of each object
@@ -16,4 +17,39 @@
     
 // really do not need to read the entire event, every time.We just need Px, and Py    
    Use a tree with one branch containing Px, another containing Py
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//________________________________________________________________
+//**** Show an Entry with TTree::Show
+//**** For example to look at the 10th entry in the staff.root tree:
+ root[] TFile f("staff.root")
+ root[] T->Show(10)
+ ======> EVENT:10
+ Category        = 361
+ Flag            = 15
+ Age             = 51
+ Service         = 29
+ Children        = 0
+ Grade           = 7
+ Step            = 13
+ Hrweek          = 40
+ Cost            = 7599
+ Division        = PS
+ Nation          = FR
+ 
+ //_________________________________________________________________________
+ //***** Scan a Variable the Tree with TTree::Scan
+ root[] T->Scan("Cost:Age:Children")
+************************************************
+*    Row   *      Cost *       Age *  Children *
+************************************************
+*        0 *     11975 *        58 *         0 *
+*        1 *     10228 *        63 *         0 *
+*        2 *     10730 *        56 *         2 *
+*        3 *      9311 *        61 *         0 *
+*        4 *      9966 *        52 *         2 *
+*        5 *      7599 *        60 *         0 *
+*        6 *      9868 *        53 *         1 *
+*        7 *      8012 *        60 *         1 *
+...
+ 
